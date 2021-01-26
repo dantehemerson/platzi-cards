9ec43707c19a30e71763c4a6569d987baacfbb9c
Dante Calderon <mail@dantecalderon.dev>
2021-01-25 23:37:02 -0500

    Fix tests

--- src/platzi-api/platzi-api.service.spec.ts
+++ src/platzi-api/platzi-api.service.spec.ts
@@ -1,12 +1,15 @@
+import { HttpModule } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlatziApiService } from './platzi-api.service';
+import { SsrDataExtractor } from './providers/ssr-data-extractor.provider';
 
 describe('PlatziApiService', () => {
   let service: PlatziApiService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PlatziApiService],
+      imports: [HttpModule],
+      providers: [PlatziApiService, SsrDataExtractor],
     }).compile();
 
     service = module.get<PlatziApiService>(PlatziApiService);
